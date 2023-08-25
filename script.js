419f8b1d9874bf7b0f231e364c4bf944962052d1
VaishnaviHonale <137199492+VaishnaviHonale@users.noreply.github.com>
2023-08-25 18:40:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ const person = {
   name: "John Doe",
   age: 80,
 };
+   
+        // Create a reference to the players array
+        let team = players;
+
+        // Create a copy of the players array
+        let team1 = [...players];
+
+        // Create a copy of the person object
+        let cap1 = { ...person };
+
+        // Testing the variables
+        console.log("Team:", team);
+        console.log("Team1:", team1);
+        console.log("Cap1:", cap1);
+
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
